69e038e1f3390f3a8feabf8b90dad0ffd547ce82
Maxim Filip <maxim@network.cz>
2026-04-01 16:53:12 -0500

    Add being able to create requirements for allies

--- rank_teams_pits.py
+++ rank_teams_pits.py
@@ -2,12 +2,26 @@ import pandas as pd
 import os
 import get_team_avg
 
-def sort_teams(criteria) -> None: # tuple in format ((Column: str, weight: float ∈ [0, 1]))
+def sort_teams(criteria=(), requirements=()) -> None: # tuple in format ((Column: str, weight: float ∈ [0, 1]))
     try:
         event_df: pd.DataFrame = pd.read_csv("data/pit_data.csv")
     except:
         raise Exception("Pit data not provided")
 
+
+
+    passes_requirements: list[bool] = [True for _ in range(len(event_df))]
+    for requirement in requirements:
+        for i in range(len(event_df)):
+            if requirement[1] == "==":
+                if event_df.loc[i, requirement[0]] != requirement[2]:
+                    passes_requirements[i] = False
+            elif requirement[1] == ">":
+                if event_df.loc[i, requirement[0]] <= requirement[2]:
+                    passes_requirements[i] = False
+            elif requirement[1] == "<":
+                if event_df.loc[i, requirement[0]] >= requirement[2]:
+                    passes_requirements[i] = False
 
     for criterium, weight in criteria:
         event_df[criterium] = event_df[criterium].astype(float)
@@ -17,8 +31,11 @@ def sort_teams(criteria) -> None: # tuple in format ((Column: str, weight: float
         best: float = top_row[criterium]
         event_df[f"{criterium}Percent"] = event_df[criterium]
         for index in range(len(event_df)):
-           event_df.loc[index, f"{criterium}Percent"] /= best
-           event_df.loc[index, f"{criterium}Percent"] *= weight
+            if passes_requirements[index]:
+                event_df.loc[index, f"{criterium}Percent"] /= best
+                event_df.loc[index, f"{criterium}Percent"] *= weight
+            else:
+                event_df.loc[index, f"{criterium}Percent"] = float('-inf')
 
     event_df["sum"] = event_df[[f"{crit[0]}Percent" for crit in criteria]].sum(axis=1)
     event_df = event_df.sort_values(by="sum", ascending=False)
@@ -27,17 +44,13 @@ def sort_teams(criteria) -> None: # tuple in format ((Column: str, weight: float
     print(event_df)
     event_df.to_csv("data/ranked_teams_pits.csv")
         
-        
-
-    
 
 def main() -> None:
-    # If data doesn't exist, generate it
-    if not os.path.exists("data/event_avg.csv"):
-        get_team_avg.main()
-        
 
-    sort_teams((("Climbing", 0.8), ("CycleTime", -0.6), ("DriveTrain", -1.2)))
+    sort_teams(
+        (("Climbing", 0.8), ("CycleTime", -0.6)), 
+        (("Score", ">", 100), ("DriveTrain", "==", 1)) # 1=SWERVE
+    ) 
 
 
 if __name__ == "__main__":
